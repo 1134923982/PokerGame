9c2d823ce92b137f9f4ffe773f11ee9ccc9197a0
yu <1134923982@qq.com>
2019-08-17 13:31:28 +0800

    [feat][create test should_return_b_player_win_when_input_1C_3D_5H_7S_9D_2C_3D_4H_5S_9C]

--- src/main/test/PokerGameTest.java
+++ src/main/test/PokerGameTest.java
@@ -8,7 +8,7 @@ import main.java.PokerGame;
 public class PokerGameTest {
 
     @Test
-    public void should_return_a_player_win_when_input_3D_5H_7S_9D_1C_3D_5H_7S_8D(){
+    public void should_return_a_player_win_when_input_1C_3D_5H_7S_9D_2C_3D_4H_5S_8D(){
         //given
         PokerGame pokerGame = new PokerGame();
         String pokers = "1C,3D,5H,7S,9D,2C,3D,4H,5S,8D";
@@ -17,7 +17,18 @@ public class PokerGameTest {
         String actualResult = pokerGame.judge(pokers);
         //then
         Assert.assertEquals(expectResult, actualResult);
+    }
 
+    @Test
+    public void should_return_b_player_win_when_input_1C_3D_5H_7S_9D_2C_3D_4H_5S_9C(){
+        //given
+        PokerGame pokerGame = new PokerGame();
+        String pokers = "1C,3D,5H,7S,9D,2C,3D,4H,7D,10C";
+        String expectResult = "b player win";
+        //when
+        String actualResult = pokerGame.judge(pokers);
+        //then
+        Assert.assertEquals(expectResult, actualResult);
     }
 
 
