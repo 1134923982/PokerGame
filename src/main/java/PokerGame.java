87a6f327cec7ff7b77491d48e2128cd37ac5a4dd
yu <1134923982@qq.com>
2019-08-17 14:25:51 +0800

    [feat][implement judge for test should_return_b_player_win_when_input_1C_3D_5H_7S_AD_2C_3D_4H_7D_9C]

--- src/main/java/PokerGame.java
+++ src/main/java/PokerGame.java
@@ -1,22 +1,53 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PokerGame {
     int pokersLength = 15;
 
     public String judge(String pokers) {
-        String[] aPlayer = pokers.substring(0,pokersLength).split(",");
-        String[] bPlayer = pokers.substring(pokersLength,pokers.length()-1).split(",");
+        String[] aPlayer = parsePokers(pokers, 0, pokersLength);
+        String[] bPlayer = parsePokers(pokers, pokersLength, pokers.length());
 
-        for(int i = aPlayer.length-1; i>=0; i--){
-            if(aPlayer[i].substring(0,1).equals(bPlayer[i].substring(0,1))){
+        for (int i = aPlayer.length - 1; i >= 0; i--) {
+            if (aPlayer[i].substring(0, aPlayer[i].length()-1).equals(bPlayer[i].substring(0, aPlayer[i].length()-1))) {
                 continue;
             }
-            if(Integer.parseInt(aPlayer[i].substring(0,1))> Integer.parseInt(bPlayer[i].substring(0,1))){
+            if (Integer.parseInt(aPlayer[i].substring(0, aPlayer[i].length()-1)) > Integer.parseInt(bPlayer[i].substring(0, bPlayer[i].length()-1))) {
                 return "a player win";
-            }else {
+            } else {
                 return "b player win";
             }
         }
         return null;
+    }
+
+    private String[] parsePokers(String pokers, int i, int pokersLength) {
+        String[] split = pokers.substring(i, pokersLength).split(",");
+
+        List<String> collect = Arrays.stream(split).map(item -> {
+            return transferred(item.substring(0, 1)) + item.substring(1, item.length());
+        }).collect(Collectors.toList());
+
+        return collect.toArray(new String[0]);
+    }
+
+    private String transferred(String str) {
+        switch (str) {
+            case "T":
+                return "10";
+            case "J":
+                return "11";
+            case "Q":
+                return "12";
+            case "K":
+                return "13";
+            case "A":
+                return "14";
+            default:
+                return str;
+        }
     }
 }
