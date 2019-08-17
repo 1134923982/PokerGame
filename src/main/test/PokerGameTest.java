0de632dcad3637a617a2c82a3559ee47c1f0f210
yu <1134923982@qq.com>
2019-08-17 13:45:59 +0800

    [feat][create test should_return_b_player_win_when_input_1C_3D_5H_7S_AD_2C_3D_4H_7D_9C]

--- src/main/test/PokerGameTest.java
+++ src/main/test/PokerGameTest.java
@@ -43,5 +43,17 @@ public class PokerGameTest {
         Assert.assertEquals(expectResult, actualResult);
     }
 
+    @Test
+    public void should_return_b_player_win_when_input_1C_3D_5H_7S_AD_2C_3D_4H_7D_9C(){
+        //given
+        PokerGame pokerGame = new PokerGame();
+        String pokers = "1C,3D,5H,7S,AD,2C,3D,4H,7D,9C";
+        String expectResult = "a player win";
+        //when
+        String actualResult = pokerGame.judge(pokers);
+        //then
+        Assert.assertEquals(expectResult, actualResult);
+    }
+
 
 }
