b2c0719d4ec1071aa159927cbeda31b96f977c29
yu <1134923982@qq.com>
2019-08-18 13:20:29 +0800

    [refactor code]

--- src/main/java/PokerGame.java
+++ src/main/java/PokerGame.java
@@ -4,10 +4,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class PokerGame {
-    int pokersLength = 15;
-    String aPlayerWin = "a player win";
-    String bPlayerWin = "b player win";
-    String peace = "peace";
+    private int pokersLength;
+    private String aPlayerWin;
+    private String bPlayerWin;
+    private String peace;
+
+    public PokerGame() {
+        aPlayerWin = "a player win";
+        bPlayerWin = "b player win";
+        peace = "peace";
+        pokersLength = 15;
+    }
 
     public String judge(String pokers) {
         String[] aPlayer = parsePokers(pokers, 0, pokersLength);
@@ -15,86 +22,140 @@ public class PokerGame {
         aPlayer = sortPokers(aPlayer);
         bPlayer = sortPokers(bPlayer);
 
-        if(isFlush(aPlayer)&&isStraight(aPlayer)&&(!isStraight(bPlayer)||!isFlush(bPlayer))){
-            return aPlayerWin;
-        }else if(isFlush(bPlayer)&&isStraight(bPlayer)&&(!isStraight(aPlayer)||!isFlush(aPlayer))){
-            return bPlayerWin;
+        String result = "";
+
+        result = getResultWithFlushAndStraight(aPlayer, bPlayer, result);
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithFourOfAKingPoker(aPlayer, bPlayer, result);
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithFullHose(aPlayer, bPlayer, result);
+
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithFlush(result, isFlush(aPlayer), !isFlush(bPlayer), !isFlush(aPlayer), isFlush(bPlayer));
+
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithStraight(aPlayer, bPlayer, result);
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithThreeOfAKings(result, aPlayer, bPlayer);
+
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithPairs(result, aPlayer, bPlayer);
+        if (result.length() > 0)
+            return result;
+
+        result = getResultWithHighCard(aPlayer, bPlayer, result);
+        return result.length() > 0 ? result : peace;
+    }
+
+    private String getResultWithStraight(String[] aPlayer, String[] bPlayer, String result) {
+        if (isStraight(aPlayer) && !isStraight(bPlayer)) {
+            result = aPlayerWin;
         }
-
-        String aFourOfAKingPoker = getRepeatOfAKingPoker(aPlayer, 4);
-        String bFourOfAKingPoker = getRepeatOfAKingPoker(bPlayer, 4);
-        if (aFourOfAKingPoker != null && bFourOfAKingPoker == null) {
-            return aPlayerWin;
-        } else if (bFourOfAKingPoker != null && aFourOfAKingPoker == null){
-            return bPlayerWin;
+        if (isStraight(bPlayer) && !isStraight(aPlayer)) {
+            result = bPlayerWin;
         }
+        return result;
+    }
 
+    private String getResultWithFlush(String result, boolean flush, boolean b, boolean b2, boolean flush2) {
+        if (flush && b) {
+            result = aPlayerWin;
+        } else {
+            if (b2 && flush2) {
+                result = bPlayerWin;
+            }
+        }
+        return result;
+    }
 
+    private String getResultWithFullHose(String[] aPlayer, String[] bPlayer, String result) {
         HashMap<String, String> aFullHose = getFullHose(aPlayer);
         HashMap<String, String> bFullHose = getFullHose(bPlayer);
 
         if ((aFullHose.get("threeOfAKingPoker") != null && aFullHose.get("pair") != null) && (bFullHose.get("pair") == null)) {
-            return aPlayerWin;
+            result = aPlayerWin;
         } else if ((bFullHose.get("threeOfAKingPoker") != null && bFullHose.get("pair") != null) && (aFullHose.get("pair") == null)) {
-            return bPlayerWin;
+            result = bPlayerWin;
         }
+        return result;
+    }
 
-        if (isFlush(aPlayer) && !isFlush(bPlayer)) {
-            return aPlayerWin;
-        } else {
-            if (!isFlush(aPlayer) && isFlush(bPlayer)) {
-                return bPlayerWin;
+    private String getResultWithFourOfAKingPoker(String[] aPlayer, String[] bPlayer, String result) {
+        String aFourOfAKingPoker = getRepeatOfAKingPoker(aPlayer, 4);
+        String bFourOfAKingPoker = getRepeatOfAKingPoker(bPlayer, 4);
+        result = getResultWithFlush(result, aFourOfAKingPoker != null, bFourOfAKingPoker == null, bFourOfAKingPoker != null, aFourOfAKingPoker == null);
+        return result;
+    }
+
+    private String getResultWithFlushAndStraight(String[] aPlayer, String[] bPlayer, String result) {
+        if (isFlush(aPlayer) && isStraight(aPlayer) && (!isStraight(bPlayer) || !isFlush(bPlayer))) {
+            result = aPlayerWin;
+        } else if (isFlush(bPlayer) && isStraight(bPlayer) && (!isStraight(aPlayer) || !isFlush(aPlayer))) {
+            result = bPlayerWin;
+        }
+        return result;
+    }
+
+    private String getResultWithHighCard(String[] aPlayer, String[] bPlayer, String result) {
+        for (int i = aPlayer.length - 1; i >= 0; i--) {
+            if (aPlayer[i].substring(0, aPlayer[i].length() - 1).equals(bPlayer[i].substring(0, aPlayer[i].length() - 1))) {
+                continue;
             }
+            result = getPokersResult(aPlayer[i], bPlayer[i]);
+            break;
         }
+        return result;
+    }
 
-        if (isStraight(aPlayer) && !isStraight(bPlayer)) {
-            return aPlayerWin;
-        }
-        if (isStraight(bPlayer) && !isStraight(aPlayer)) {
-            return bPlayerWin;
-        }
-
-        String aThreeOfAKingPoker = getRepeatOfAKingPoker(aPlayer, 3);
-        String bThreeOfAKingPoker = getRepeatOfAKingPoker(bPlayer, 3);
-        if (aThreeOfAKingPoker != null || bThreeOfAKingPoker != null) {
-            if (aThreeOfAKingPoker != null && bThreeOfAKingPoker == null) {
-                return aPlayerWin;
-            } else if (aThreeOfAKingPoker == null && bThreeOfAKingPoker != null) {
-                return bPlayerWin;
-            } else {
-                if (Integer.parseInt(aThreeOfAKingPoker) > Integer.parseInt(bThreeOfAKingPoker)) {
-                    return aPlayerWin;
-                } else {
-                    return bPlayerWin;
-                }
-            }
-        }
-
+    private String getResultWithPairs(String result, String[] aPlayer, String[] bPlayer) {
         List<String> aPlayersPairs = getPairs(aPlayer);
         List<String> bPlayersPairs = getPairs(bPlayer);
-
         if (aPlayersPairs.size() > 0 || bPlayersPairs.size() > 0) {
             if (aPlayersPairs.size() > bPlayersPairs.size()) {
-                return aPlayerWin;
+                result = aPlayerWin;
             } else if (aPlayersPairs.size() < bPlayersPairs.size()) {
-                return bPlayerWin;
+                result = bPlayerWin;
             } else {
                 for (int i = 0; i < aPlayersPairs.size(); i++) {
                     if (aPlayersPairs.get(i).substring(0, aPlayersPairs.get(i).length() - 1).equals(bPlayersPairs.get(i).substring(0, bPlayersPairs.get(i).length() - 1))) {
                         continue;
                     }
-                    return getPokersResult(aPlayersPairs.get(i), bPlayersPairs.get(i));
+                    result = getPokersResult(aPlayersPairs.get(i), bPlayersPairs.get(i));
                 }
             }
         }
+        return result;
+    }
 
-        for (int i = aPlayer.length - 1; i >= 0; i--) {
-            if (aPlayer[i].substring(0, aPlayer[i].length() - 1).equals(bPlayer[i].substring(0, aPlayer[i].length() - 1))) {
-                continue;
+    private String getResultWithThreeOfAKings(String result, String[] aPlayer, String[] bPlayer) {
+        String aThreeOfAKingPoker = getRepeatOfAKingPoker(aPlayer, 3);
+        String bThreeOfAKingPoker = getRepeatOfAKingPoker(bPlayer, 3);
+        if (aThreeOfAKingPoker != null || bThreeOfAKingPoker != null) {
+            if (aThreeOfAKingPoker != null && bThreeOfAKingPoker == null) {
+                result = aPlayerWin;
+            } else if (aThreeOfAKingPoker == null && bThreeOfAKingPoker != null) {
+                result = bPlayerWin;
+            } else {
+                if (Integer.parseInt(aThreeOfAKingPoker) > Integer.parseInt(bThreeOfAKingPoker)) {
+                    result = aPlayerWin;
+                } else {
+                    result = bPlayerWin;
+                }
             }
-            return getPokersResult(aPlayer[i], bPlayer[i]);
         }
-        return peace;
+        return result;
     }
 
     private HashMap<String, String> getFullHose(String[] pokers) {
